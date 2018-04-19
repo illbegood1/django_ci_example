5c112d1f95998c95147293b7217c8d3af47e6066
illbegood1 <38519082+illbegood1@users.noreply.github.com>
2018-04-19 02:56:47 -0400

    Update wsgi.py

--- django_ci_example/wsgi.py
+++ django_ci_example/wsgi.py
@@ -1,3 +1,6 @@
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
 """
 WSGI config for django_ci_example project.
 
@@ -9,8 +12,5 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_ci_example.settings.dev")
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 application = Cling(get_wsgi_application())
