24358ee821b28407b40636e6387a92cd97fae94d
illbegood1 <38519082+illbegood1@users.noreply.github.com>
2018-04-19 02:25:03 -0400

    Update heroku.py

--- django_ci_example/settings/heroku.py
+++ django_ci_example/settings/heroku.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 from .base import *  # noqa
 
-base.DATABASES['default'] = dj_database_url.config()
+.base.DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
